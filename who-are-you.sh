0fbac090499f2d8ed4cf5bde52094b5af9dae1de
yhekkour <yhekkouri@gmail.com>
2024-01-09 10:17:59 +0000

    who-are-you

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,1 +1,1 @@
-curl https://learn.zone01oujda.ma/assets/superhero/all.json | jq  ' .[] | select( .id == 70) | .name '$
+curl https://learn.zone01oujda.ma/assets/superhero/all.json | jq  ' .[] | select( .id == 70) | .name '
