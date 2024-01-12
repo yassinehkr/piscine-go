c30510ff27268cf4d8d2104cb2b4b6fbec2ee6a7
Yassine HEKKOURI <yhekkour@noreply.learn.zone01oujda.ma>
2024-01-12 11:50:38 +0000

    aaaa

--- divmod.go
+++ divmod.go
@@ -1,17 +1,7 @@
 package piscine
 
 func DivMod(a int, b int, div *int, mod *int) {
-
-	a := 13
-	b := 2
-
-	div := a / b
-
-	a := 13
-	b := 2
-
-	mod := a % b
-
-	fmt.Println(div)
-	fmt.Println(mod)
+	*div = a / b
+	*mod = a % b
 }
+
