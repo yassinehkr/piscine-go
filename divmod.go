15a957f6e051c7dfc3046d432d8bb4979d754f6e
Yassine HEKKOURI <yhekkour@noreply.learn.zone01oujda.ma>
2024-01-12 11:41:20 +0000

    f

---
+++ divmod.go
@@ -0,0 +1,17 @@
+package piscine
+
+func DivMod(a int, b int, div *int, mod *int) {
+
+	a := 13
+	b := 2
+
+	div := a / b
+
+	a := 13
+	b := 2
+
+	mod := a % b
+
+	fmt.Println(div)
+	fmt.Println(mod)
+}
