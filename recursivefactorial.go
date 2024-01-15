031bf5784b04d1df013225eb419c9e7e68f466b3
yhekkour <yhekkouri@gmail.com>
2024-01-15 14:22:24 -0500

    MyRecursiveFactorialProject

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,9 +1,11 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb == 1 || nb == 0 {
+	if nb > 1 && nb < 2147483647 {
+		return RecursiveFactorial(nb-1) * nb
+	} else if nb == 1 || nb == 0 {
 		return 1
+	} else {
+		return 0
 	}
-	return nb * RecursiveFactorial(nb-1)
-
 }
