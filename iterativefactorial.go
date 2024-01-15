8b4952e519675e287f0b5abdc960cf8caa9fa5be
yhekkour <yhekkouri@gmail.com>
2024-01-15 13:36:33 -0500

    Factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,10 +1,15 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb == 1 {
+	if nb > 1 && nb < 2147483647 {
+		r := 1
+		for i := nb; i >= 1; i-- {
+			r *= i
+		}
+		return int(r)
+	} else if nb == 0 || nb == 1 {
 		return 1
-	} else if nb >= 24 {
+	} else {
 		return 0
 	}
-	return nb * IterativeFactorial(nb-1)
 }
