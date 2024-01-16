4ac9719711bdc06d2f7271283686a1f600b78cd9
yhekkour <yhekkouri@gmail.com>
2024-01-16 11:50:08 +0000

    myRecursivePower

---
+++ recursivepower.go
@@ -0,0 +1,11 @@
+package piscine
+
+func recursivepower(nb int, power int) int {
+	if power < 0 {
+		return 0
+	} else if power == 0 {
+		return 1
+	} else {
+		return nb * recursivepower(nb, power-1)
+	}
+}
