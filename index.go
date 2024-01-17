12d33a431c60ef61369fc9ed0cbaeb296d91e86e
yhekkour <yhekkouri@gmail.com>
2024-01-17 15:47:17 +0000

    myIndex

---
+++ index.go
@@ -0,0 +1,14 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	if len(toFind) == 0 {
+		return 0
+	}
+	for x := 0; x <= len(s)-len(toFind); x++ {
+		y := s[i : x+len(toFind)]
+		if toFind == y {
+			return x
+		}
+	}
+	return -1
+}
