8b678d8659f4e9be34f22d5e9c2c93fc660a8901
yhekkour <yhekkouri@gmail.com>
2024-01-17 14:04:16 +0000

    myAlphacount

---
+++ alphacount.go
@@ -0,0 +1,14 @@
+package piscine
+
+func AlphaCount(s string) int {
+	r := []rune(s)
+	x := 0
+	for i := 0; i < len(r); i++ {
+		if r[i] >= 'a' && r[i] <= 'z' || r[i] >= 'A' && r[i] <= 'Z' {
+			x++
+
+		}
+
+	}
+	return x
+}
