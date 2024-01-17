c56e64fb3a95811e49a6f9bcc42e5d82e55f4627
Yassine HEKKOURI <yhekkour@noreply.learn.zone01oujda.ma>
2024-01-17 14:07:21 +0000

    Update 'alphacount.go'
    Signed-off-by: Yassine HEKKOURI <yhekkour@noreply.learn.zone01oujda.ma>

--- alphacount.go
+++ alphacount.go
@@ -4,7 +4,7 @@ func AlphaCount(s string) int {
 	r := []rune(s)
 	x := 0
 	for i := 0; i < len(r); i++ {
-		if r[i] >= 'a' && r[i] <= 'z' || r[i] >= 'A' && r[i] <= 'Z' {
+		if (r[i] >= 'a' && r[i] <= 'z' || r[i] >= 'A' && r[i] <= 'Z') {
 			x++
 
 		}
