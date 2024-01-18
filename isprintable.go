4c906118ce12e7dec450ade11d7346bcb6f86d8e
yhekkour <yhekkouri@gmail.com>
2024-01-18 03:40:34 +0000

    MyIsprintable

--- isprintable.go
+++ isprintable.go
@@ -3,7 +3,7 @@ package piscine
 func IsPrintable(s string) bool {
 	r := []rune(s)
 	for x := 0; x < len(s); x++ {
-		if r[x] < 32 || r[x] > 126 {
+		if r[x] <= 32 || r[x] >= 126 {
 			return true
 		}
 	}
