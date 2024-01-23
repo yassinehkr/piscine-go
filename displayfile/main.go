f35d1958f2a10dadc1b3a844695578b7e046e02c
yhekkour <yhekkouri@gmail.com>
2024-01-23 16:06:55 +0100

    Display File

--- displayfile/main.go
+++ displayfile/main.go
@@ -10,13 +10,13 @@ func main() {
 	s := os.Args
 
 	if len(s) > 2 {
-		fmt.Printf("Too many arguments")
+		fmt.Println("Too many arguments")
 	} else if len(s) < 2 {
-		fmt.Printf("File name missing")
+		fmt.Println("File name missing")
 	} else {
 		content, err := ioutil.ReadFile(os.Args[1])
 		if err != nil {
-			fmt.Printf(err.Error())
+			fmt.Println(err.Error())
 		} else {
 			fmt.Printf(string(content))
 		}
