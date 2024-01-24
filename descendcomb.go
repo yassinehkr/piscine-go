6b1d86ac35aeebe234491d76f75fa03a3d488e19
yhekkour <yhekkouri@gmail.com>
2024-01-24 12:19:56 +0100

    Descend Comb

---
+++ descendcomb.go
@@ -0,0 +1,28 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func DescendComb() {
+	for x := '9'; x <= '0'; x++ {
+		for y := '9'; y <= '0'; y++ {
+			for i := '9'; i <= '0'; i++ {
+				for j := '9'; j <= '0'; j++ {
+					if (x < i) || (x == i && y < j) {
+						z01.PrintRune(x)
+						z01.PrintRune(y)
+						z01.PrintRune(' ')
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+
+						if x == '0' && y == '1' && i == '0' && j == '0' {
+							z01.PrintRune('\n')
+						} else {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
+				}
+			}
+		}
+	}
+}
