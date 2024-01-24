036116ab8d718da47caacfa0958405b9c1737a94
yhekkour <yhekkouri@gmail.com>
2024-01-24 17:57:12 +0100

    shoppingsummarycounter

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,23 +1,17 @@
 package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
-	summary := make(map[string]int)
-	start := 0
-
-	for i := 0; i < len(str); i++ {
-		if str[i] == ' ' {
-			if start < i {
-				item := str[start:i]
-				summary[item]++
-			}
-			start = i + 1
+	list := make(map[string]int)
+	var items string
+	for _, e := range str {
+		if e == 32 {
+			list[items] += 1
+			items = ""
+		} else if e != 32 {
+			items += string(byte(e))
 		}
 	}
+	list[items] += 1
 
-	if start < len(str) {
-		item := str[start:]
-		summary[item]++
-	}
-
-	return summary
+	return list
 }
