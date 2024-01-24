218fa59b3bb0242b0695e64924f00ada12994aac
yhekkour <yhekkouri@gmail.com>
2024-01-24 13:50:12 +0100

    unmatch

--- unmatch.go
+++ unmatch.go
@@ -1,17 +1,19 @@
 package piscine
 
-func Unmatch(a []int) int {
-	for i := 0; i < len(a); i++ {
-		found := false
-		for j := 0; j < len(a); j++ {
-			if i != j && a[i] == a[j] {
-				found = true
-				break
+func Unmatch(arr []int) int {
+	for i := 0; i < len(arr); i++ {
+		c := 0
+
+		for j := 0; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				c++
 			}
 		}
-		if !found {
-			return a[i]
+
+		if c%2 != 0 {
+			return arr[i]
 		}
 	}
+
 	return -1
 }
