4799a21efe3e207d489adc2e4d1d7f42254a2753
yhekkour <yhekkouri@gmaol.com>
2024-01-24 19:56:00 +0100

    dealapackofcards

---
+++ dealapackofcards.go
@@ -0,0 +1,18 @@
+package piscine
+
+import "fmt"
+
+func DealAPackOfCards(deck []int) {
+	for i := 0; i < 12; i += 3 {
+		player := i/3 + 1
+		end := i + 3
+		fmt.Printf("Player %d: ", player)
+		for i, card := range deck[i:end] {
+			fmt.Printf("%d", card)
+			if i != 2 {
+				fmt.Printf(", ")
+			}
+		}
+		fmt.Printf("\n")
+	}
+}
