bd6a097be95e0083d515ad573a4cafd2fe7b42c3
yhekkour <yhekkouri@gmaol.com>
2024-01-24 21:40:33 +0100

    string to int slice

--- stringtointslice.go
+++ stringtointslice.go
@@ -1,17 +1,9 @@
-package main
-
-import "fmt"
+package piscine
 
 func StringToIntSlice(str string) []int {
-	var result []int
+	result := []int{}
 	for _, v := range str {
 		result = append(result, int(v))
 	}
 	return result
-}
-
-func main() {
-	fmt.Println(StringToIntSlice("A quick brown fox jumps over the lazy dog"))
-	fmt.Println(StringToIntSlice("Converted this string into an int"))
-	fmt.Println(StringToIntSlice("hello THERE"))
 }
