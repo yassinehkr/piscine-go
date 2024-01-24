782dd789099f63cbb6e7be252c7e865346e5f92f
yhekkour <yhekkouri@gmail.com>
2024-01-24 14:48:29 +0100

    fooddeliverytime

---
+++ fooddeliverytime.go
@@ -0,0 +1,19 @@
+package piscine
+
+type food struct {
+	preptime int
+}
+
+func FoodDeliveryTime(order string) int {
+	menu := map[string]food{
+		"burger":  {preptime: 15},
+		"chips":   {preptime: 10},
+		"nuggets": {preptime: 12},
+	}
+
+	i, res := menu[order]
+	if !res {
+		return 404
+	}
+	return i.preptime
+}
