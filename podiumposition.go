b80e7bf51b754c8a90bf6385da8397c8fcad5aa5
yhekkour <yhekkouri@gmaol.com>
2024-01-25 04:40:44 +0100

    podium position

--- podiumposition.go
+++ podiumposition.go
@@ -1,6 +1,12 @@
 package piscine
 
 func PodiumPosition(podium [][]string) [][]string {
-	podium[0], podium[1], podium[2], podium[3] = podium[3], podium[2], podium[1], podium[0]
+	for i := 0; i < len(podium)-1; i++ {
+		for j := 0; j < len(podium)-i-1; j++ {
+			if podium[j][0] > podium[j+1][0] {
+				podium[j], podium[j+1] = podium[j+1], podium[j]
+			}
+		}
+	}
 	return podium
 }
